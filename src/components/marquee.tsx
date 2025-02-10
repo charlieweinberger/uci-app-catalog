2a62c464cf3564f90025da7e6bbb6496d9310d17
charlieweinberger <charlieweinberger05@gmail.com>
2025-02-10 00:14:31 -0800

    reverted change

--- src/components/marquee.tsx
+++ src/components/marquee.tsx
@@ -7,7 +7,7 @@ import { websites } from "@/lib/mock-data";
 export default function WebsiteMarquee() {
   // TODO: make the gallery items unclickable (other than the link) in the marquee
   return (
-    <Marquee className="max-w-[1536px]">
+    <Marquee className="max-w-[1536px]" pauseOnHover>
       {websites.map((website) => (
         <GalleryItem
           key={website.id}
