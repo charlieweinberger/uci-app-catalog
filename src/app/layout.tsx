35e8a8f69581daab47f7457f2a1705339517ae35
charlieweinberger <charlieweinberger05@gmail.com>
2025-02-21 21:43:46 -0800

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type React from "react";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+
 import "@/app/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -23,6 +26,7 @@ export default function RootLayout({
             {children}
           </main>
         </div>
+        <Analytics />
       </body>
     </html>
   );
