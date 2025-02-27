48565ab32857f78a9e09571f01f89824f43b2d63
charlieweinberger <charlieweinberger05@gmail.com>
2025-02-26 22:25:30 -0800

    fixed image height

--- src/components/WebsiteImage.tsx
+++ src/components/WebsiteImage.tsx
@@ -5,13 +5,13 @@ export default function WebsiteImage({ website, type }: {
   type: ImageType
 }) {
   return (
-    <div className={`h-${(type === "gallery") ? 40 : 72} flex flex-row justify-center`}>
+    <div className="flex flex-row justify-center">
       <Image
         src={website.screenshot}
         alt={`Screenshot of ${website.name}`}
         width={1000}
         height={1000}
-        className="w-auto max-w-full h-auto max-h-full object-cover rounded-xl border-uci-blue border-4"
+        className={`w-auto h-${(type === "gallery") ? 40 : 80} object-cover rounded-xl border-uci-blue border-4`}
       />
     </div>
   );
